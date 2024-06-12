acedf5a91f08509e1af9752ee9654e7c754f4dfa
Filipe Almeida <filipe@filipe>
2024-06-12 01:33:05 -0300

    Adding README file

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output ami_id {
+output "ami_id" {
   description = "The EC2 AMD ID retrieved on data source type"
   value       = data.aws_ami.ubuntu.id
   sensitive   = false
--- variables.tf
+++ variables.tf
@@ -12,23 +12,23 @@ variable "aws_region" {
 
 variable "public_subnet" {
   description = "The public subnet to be used on EC2 Webserver instance"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
   description = "The EC2 instance type"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "Default project name"
-  type    = string
-  default = "Aula 01"
+  type        = string
+  default     = "Aula 01"
 }
 
 variable "default_cidr_block" {
   description = "Default IPv4 outbound cidr block - 0.0.0.0/0"
-  type = string
+  type        = string
 }
 
 variable "vpc_cidr" {
